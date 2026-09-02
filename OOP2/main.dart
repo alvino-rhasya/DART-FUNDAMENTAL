43578250e6e394ecdb7154981933aade2ed59714
alvino-rhasya <alvino051109@gmail.com>
2026-09-02 10:55:24 +0700

    Add Plants class with attributes and constructor for plant initialization

--- OOP2/main.dart
+++ OOP2/main.dart
@@ -1,8 +1,10 @@
 import 'animal.dart';
+import 'plants.dart';
 
 void main(List<String> args) {
   // var namaObjek = namaClass(properties)
   var ucupCat = Animal('Ucup Guerrero', 'Orange', 4.2);
+  var ucupRose = Plants('Rose', 'Derrick Rose', 'White');
 
   ucupCat.eat();
   print(ucupCat.weight);
--- OOP2/plants.dart
+++ OOP2/plants.dart
@@ -1,3 +1,19 @@
 class Plants {
-  
+  //attribute/properties
+  String? name;
+  String? latinName;
+  String? color;
+
+  //Constructor
+  /**
+   * membuat inisialisasi lebih mudah daripada satu/satu
+   * bisa juga di sebut sebagai methods khusus 
+   * karena berbeda dengan methods biasa 
+   * 1. nama harus sama dengan nama class
+   * 2. tidak memiliki kembalian (return type)
+   * 3. dipanggil otomatis ketika objek dibuat 
+   * 4. digunakan untuk inisialisasi objek
+   */
+
+  Plants(this.name, this.latinName, this.color);
 }
