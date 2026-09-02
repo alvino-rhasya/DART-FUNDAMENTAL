7ddf2ae95914b7ca239abbe8ee70c6f20afc46df
alvino-rhasya <alvino051109@gmail.com>
2026-09-02 11:08:55 +0700

    Update comments for clarity on object initialization and cascade notation

--- OOP2/main.dart
+++ OOP2/main.dart
@@ -2,10 +2,12 @@ import 'animal.dart';
 import 'plants.dart';
 
 void main(List<String> args) {
-  // var namaObjek = namaClass(properties)
+  // var namaObjek = namaClass(properties/parameter)
   var ucupCat = Animal('Ucup Guerrero', 'Orange', 4.2);
   var ucupRose = Plants('Rose', 'Derrick Rose', 'White');
 
   ucupCat.eat();
   print(ucupCat.weight);
+
+  // cascade notation ditandai dengan ..
 }
